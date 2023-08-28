04905c107231c678ec64e367e43a117f9c62ac36
Daniil Kulikovskiy <danilkulikovskiy@gmail.com>
2023-08-28 14:39:29 +0300

    completed layout BookmarksCell

--- BestRecipes/Screens/SavedRecipesView.swift
+++ BestRecipes/Screens/SavedRecipesView.swift
@@ -8,73 +8,14 @@
 import SwiftUI
 
 struct SavedRecipesView: View {
-    var title: String = "How to sharwama at home"
-    var subtitle: String = "Subtitle"
-    var image = "Food Thumbnail"
-    var autorImage = "author"
-    var autorName = "Zeelicious foods"
-    var scoreNumber: Double = 5.0
-    
     var body: some View {
-        VStack(alignment: .center) {
-            ZStack(alignment: .top) {
-                Image(image)
-                HStack {
-                    Button {
-                        
-                    } label: {
-                        Image(systemName: "star.fill")
-                            .foregroundColor(.black)
-                        Text(String(scoreNumber))
-                            .font(.system(.headline).bold())
-                            .foregroundColor(.white)
-                    }
-                    .frame(width: 58, height: 27.6)
-                    .background(.ultraThinMaterial)
-                    .background(.black)
-                    .clipShape(Capsule())
-                    Spacer()
-                    
-                    Button {
-                    } label: {
-                        Image(systemName: "bookmark")
-                            .frame(width: 32, height: 32)
-                            .foregroundColor(.red)
-                    }
-                    .background(.ultraThinMaterial)
-                    .background(.white)
-                    .clipShape(Circle())
-                }
-                .padding(.top, 10)
-                .padding([.leading, .trailing], 33)
+        NavigationView {
+            VStack(spacing: 30) {
+                BookmarksCell()
+                BookmarksCell()
             }
-            VStack(alignment: .leading) {
-                HStack{
-                    Text(title)
-                        .font(.headline)
-                        .padding(.top, 2)
-                    Spacer()
-                    Button {
-                        tapShare()
-                    } label: {
-                        Image(systemName: "ellipsis")
-                            .foregroundColor(.black)
-                    }
-                }
-                .padding(.top, 5)
-                .padding([.leading, .trailing], 30)
-                HStack() {
-                    Image(autorImage)
-                    Text("By \(autorName)")
-                        .padding(.leading, 5)
-                        .foregroundColor(.gray)
-                }
-                .padding(.leading, 30)
-            }
+            .navigationTitle("Saved recipes")
         }
-    }
-    private func tapShare() {
-        print("Share button tap!")
     }
 }
 
