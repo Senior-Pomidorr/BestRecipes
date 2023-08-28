2ad46db6ebc0c784a5baf235672ba302acb4623d
Daniil Kulikovskiy <danilkulikovskiy@gmail.com>
2023-08-28 08:56:46 +0300

    add bookmarks cell layout

--- BestRecipes/Screens/SavedRecipesView.swift
+++ BestRecipes/Screens/SavedRecipesView.swift
@@ -8,8 +8,73 @@
 import SwiftUI
 
 struct SavedRecipesView: View {
+    var title: String = "How to sharwama at home"
+    var subtitle: String = "Subtitle"
+    var image = "Food Thumbnail"
+    var autorImage = "author"
+    var autorName = "Zeelicious foods"
+    var scoreNumber: Double = 5.0
+    
     var body: some View {
-        Text("SavedRecipesView")
+        VStack(alignment: .center) {
+            ZStack(alignment: .top) {
+                Image(image)
+                HStack {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "star.fill")
+                            .foregroundColor(.black)
+                        Text(String(scoreNumber))
+                            .font(.system(.headline).bold())
+                            .foregroundColor(.white)
+                    }
+                    .frame(width: 58, height: 27.6)
+                    .background(.ultraThinMaterial)
+                    .background(.black)
+                    .clipShape(Capsule())
+                    Spacer()
+                    
+                    Button {
+                    } label: {
+                        Image(systemName: "bookmark")
+                            .frame(width: 32, height: 32)
+                            .foregroundColor(.red)
+                    }
+                    .background(.ultraThinMaterial)
+                    .background(.white)
+                    .clipShape(Circle())
+                }
+                .padding(.top, 10)
+                .padding([.leading, .trailing], 33)
+            }
+            VStack(alignment: .leading) {
+                HStack{
+                    Text(title)
+                        .font(.headline)
+                        .padding(.top, 2)
+                    Spacer()
+                    Button {
+                        tapShare()
+                    } label: {
+                        Image(systemName: "ellipsis")
+                            .foregroundColor(.black)
+                    }
+                }
+                .padding(.top, 5)
+                .padding([.leading, .trailing], 30)
+                HStack() {
+                    Image(autorImage)
+                    Text("By \(autorName)")
+                        .padding(.leading, 5)
+                        .foregroundColor(.gray)
+                }
+                .padding(.leading, 30)
+            }
+        }
+    }
+    private func tapShare() {
+        print("Share button tap!")
     }
 }
 
---
+++ BestRecipes/Views/BookmarksCell.swift
@@ -0,0 +1,25 @@
+//
+//  BookmarksCell.swift
+//  BestRecipes
+//
+//  Created by Daniil Kulikovskiy on 27.08.2023.
+//
+
+import SwiftUI
+
+struct BookmarksCell: View {
+    var title: String
+    var subtitle: String
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(title)
+                .font(.headline)
+            Text(subtitle)
+                .font(.subheadline)
+                .foregroundColor(.gray)
+        }
+        .padding()
+    }
+}
+
