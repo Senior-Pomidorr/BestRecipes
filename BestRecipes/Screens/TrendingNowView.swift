3d8209ab2cdf1cecb7ee17c541654fa3143d0762
Liliya (Tacarlen) <l.feodotova296@yandex.ru>
2023-09-07 12:37:40 +0300

    TrendingNowView fix scroll

--- BestRecipes/Screens/TrendingNowView.swift
+++ BestRecipes/Screens/TrendingNowView.swift
@@ -11,38 +11,41 @@ struct TrendingNowView: View {
     @EnvironmentObject var networkAggregateModel: NetworkAggregateModel
     
     var body: some View {
-        ScrollView {
-            ForEach(networkAggregateModel.shortRecipeListTrendingNow, id: \.self) { recipe in
-                let recipeID = recipe.id ?? 0
-                NavigationLink(destination: DetailRecipeView(recipeID: String(recipeID))) {
-                    LazyVStack {
-                        TrendingNowCell(title: recipe.title ?? "detailRecipe.nonameofrecipe".localized,
-                                        subtitle: "Subtitle",
-                                        image: recipe.image ?? "",
-                                        scoreNumber: 5.0,
-                                        ingredintsCount: 9,
-                                        receptMinutes: 25)
+        GeometryReader{ geometry in
+            ScrollView {
+                ForEach(networkAggregateModel.shortRecipeListTrendingNow, id: \.self) { recipe in
+                    let recipeID = recipe.id ?? 0
+                    NavigationLink(destination: DetailRecipeView(recipeID: String(recipeID))) {
+                        LazyVStack {
+                            TrendingNowCell(title: recipe.title ?? "detailRecipe.nonameofrecipe".localized,
+                                            subtitle: "Subtitle",
+                                            image: recipe.image ?? "",
+                                            scoreNumber: 5.0,
+                                            ingredintsCount: 9,
+                                            receptMinutes: 25)
+                        }
+                        .padding(.bottom, 16)
                     }
-                    .padding(.bottom, 16)
                 }
-            }
-            .padding(.vertical, 16)
-            .task {
-                networkAggregateModel.searchRecipeShort(params: ["sort":"popularity"], requestTag: .trendingNow)
-                print(networkAggregateModel.shortRecipeListTrendingNow)
-            }
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbar {
-                ToolbarItem(placement: .principal) {
-                    VStack {
-                        Text("Trending Now")
-                            .font(.custom(Poppins.SemiBold, size: 24))
-                            .foregroundColor(.black)
+                .padding(.vertical, 16)
+                .task {
+                    networkAggregateModel.searchRecipeShort(params: ["sort":"popularity"], requestTag: .trendingNow)
+                    print(networkAggregateModel.shortRecipeListTrendingNow)
+                }
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .principal) {
+                        VStack {
+                            Text("Trending Now")
+                                .font(.custom(Poppins.SemiBold, size: 24))
+                                .foregroundColor(.black)
+                        }
                     }
                 }
             }
             .navigationBarBackButtonHidden(true)
             .navigationBarItems(leading: CustomBackButton())
+            .padding(.bottom, geometry.safeAreaInsets.bottom)
         }
     }
     
